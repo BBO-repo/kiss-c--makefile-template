05e04ee7729c6237f7e2dee395acd24cbbb2eb32
BBO <bdz_brahim@yahoo.fr>
2019-08-30 16:18:54 +0200

    Update plot.cpp

--- source/plot.cpp
+++ source/plot.cpp
@@ -3,11 +3,7 @@
 
 int main(int argc, char ** argv) {
   // call a function in another file
-  myPrintHelloMake();
-  feed();
-  
-  x00 *x = new x00( argc, argv );
-  delete x;
+  helloMake();
 	
   return(0);
 }
